10b95b7669307178595de469b91bbc20be61cd56
Luis Martínez <wifly.sk8@gmail.com>
2022-04-01 11:24:40 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { GestionPageComponent } from './pages/gestion-page/gestion-page.component';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomePageComponent},
+  {path: "products", component: ProductsPageComponent},
+  {path: "product", redirectTo: "products"},
+  {path: "gestion", component: GestionPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
